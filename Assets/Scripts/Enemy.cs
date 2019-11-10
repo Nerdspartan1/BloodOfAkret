dd2ba82db06d5d12f3f0959fb3117eb1d82906a1
Olivier <o.cornet2@gmail.com>
2019-11-10 16:26:28 +0100

    Add enemy basic models and prefabs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : vp_DamageHandler
+{
+	public GameObject Target;
+	public GameObject Ragdoll;
+
+	private NavMeshAgent _nav;
+	private Animator _anim;
+	private float _lastDamageTaken;
+
+    void Start()
+    {
+		_nav = GetComponent<NavMeshAgent>();
+		_anim = GetComponent<Animator>();
+		foreach (Rigidbody rb in Ragdoll.GetComponentsInChildren<Rigidbody>())
+		{
+			rb.isKinematic = true;
+		}
+	}
+
+	public override void Damage(vp_DamageInfo damageInfo)
+	{
+		_lastDamageTaken = damageInfo.Damage;
+		base.Damage(damageInfo);
+	}
+
+	public override void Die()
+	{
+		Vector3 pushForce = 10f*(transform.position - Target.transform.position).normalized * _lastDamageTaken;
+		foreach (Rigidbody rb in Ragdoll.GetComponentsInChildren<Rigidbody>())
+		{
+			rb.isKinematic = false;
+			rb.AddForce(pushForce,ForceMode.Impulse);
+		}
+		_nav.enabled = false;
+		_anim.enabled = false;
+		this.enabled = false;
+	}
+
+	void Update()
+    {
+		if(Target) _nav.SetDestination(Target.transform.position);
+    }
+}
