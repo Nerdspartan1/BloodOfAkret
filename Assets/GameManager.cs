d77c2be488c298e868519e505ec70c59896c8e00
Olivier <o.cornet2@gmail.com>
2019-11-10 19:35:44 +0100

    Add menu and placeholder arena

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject Player;
+	public GameObject IntroSpawnLocation;
+	public GameObject GameSpawnLocation;
+
+	public GameObject Menu;
+	public GameObject Intro;
+	public GameObject Game;
+
+    void Start()
+    {
+		Menu.SetActive(true);
+		Player.SetActive(false);
+		Intro.SetActive(false);
+		Game.SetActive(false);
+    }
+
+	public void StartIntro()
+	{
+		Menu.SetActive(false);
+		Player.transform.position = IntroSpawnLocation.transform.position;
+		Intro.SetActive(true);
+		Player.SetActive(true);
+	}
+
+	public void StartGame()
+	{
+		Menu.SetActive(false);
+		Player.transform.position = GameSpawnLocation.transform.position;
+		Game.SetActive(true);
+		Player.SetActive(true);
+	}
+}
