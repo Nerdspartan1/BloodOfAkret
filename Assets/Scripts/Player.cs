94b2c3ff0487113ac726b3cbabe78fd5d9a8f5a7
Olivier <o.cornet2@gmail.com>
2019-11-14 19:04:39 +0100

    Add speed perk

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum Perk
+{
+	DemonLegs,
+}
+
+public class Player : vp_PlayerDamageHandler
+{
+	private vp_FPController _controller;
+	public List<Perk> Perks;
+
+	private float _baseDamping;
+	private float _baseJumpForce;
+
+	private void Start()
+	{
+		_controller = GetComponent<vp_FPController>();
+		_baseDamping = _controller.MotorDamping;
+		_baseJumpForce = _controller.MotorJumpForce;
+	}
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.T)) PerkUp(Perk.DemonLegs);
+	}
+
+
+	public void PerkUp(Perk perk)
+	{
+		Perks.Add(perk);
+
+		int dl = Perks.Count(p => p == Perk.DemonLegs);
+		_controller.MotorDamping = _baseDamping*Mathf.Pow(1f-0.25f,dl);
+		_controller.MotorJumpForce = _baseJumpForce * Mathf.Pow(1.25f, dl);
+	}
+
+
+}
