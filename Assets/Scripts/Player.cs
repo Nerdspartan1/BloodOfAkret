6adbeb8d7d236b159abae878f35496c19641884b
Olivier <o.cornet2@gmail.com>
2019-11-14 22:36:35 +0100

    Add gun emplacement and infinite ammo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,10 @@ public enum Perk
 public class Player : vp_PlayerDamageHandler
 {
 	private vp_FPController _controller;
+	private vp_PlayerInventory _inventory;
 	public List<Perk> Perks;
+
+	public vp_UnitType InfiniteAmmo;
 
 	private float _baseDamping;
 	private float _baseJumpForce;
@@ -21,6 +24,9 @@ public class Player : vp_PlayerDamageHandler
 		_controller = GetComponent<vp_FPController>();
 		_baseDamping = _controller.MotorDamping;
 		_baseJumpForce = _controller.MotorJumpForce;
+		_inventory = GetComponent<vp_PlayerInventory>();
+		_inventory.TryGiveUnits(InfiniteAmmo,int.MaxValue);
+
 	}
 
 	public void Update()
